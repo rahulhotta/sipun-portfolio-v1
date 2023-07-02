5fcdbb3f3de1057d24e401590e715b73ea809350
Mishrani <49362029+Mishrani@users.noreply.github.com>
2023-07-02 20:44:41 +0530

    capital Home

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 import Navbar from '../Components/navigation/Navbar';
-import HomePage from '@/Components/home/home';
+import HomePage from '@/Components/Home/Home';
 import styles from './page.module.css';
 
 export default function Home() {
