f298e359e89d32f9c5dc70623272ca2e7770e13b
rahulhotta <rahulhotta99@gmail.com>
2023-07-30 11:43:19 +0530

    fixed some bugs

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import Navbar from '../../Components/navigation/Navbar';
-import styles from './About.module.css';
-import Image from 'next/image';
-import about_image from '../../Assets/about_image-removebg.png';
-import SkillCard from '@/Components/skills/SkillCard';
-import { FaJava, FaGitSquare } from 'react-icons/fa';
-import { TbSql } from 'react-icons/tb';
-import { AiFillHtml5, AiFillGitlab } from 'react-icons/ai';
-import { SiSelenium, SiJira, SiBlazemeter } from 'react-icons/si';
-import jmeter from '../../Assets/skill-logos/Jmeter.png';
-import selenium from '../../Assets/skill-logos/selenium.png';
-import sql from '../../Assets/skill-logos/sql.png';
-import git from '../../Assets/skill-logos/git.png';
-import appium from '../../Assets/skill-logos/Appium.png';
-import jenkins from '../../Assets/skill-logos/jenkins.png';
-import rpa from '../../Assets/skill-logos/rpa.png';
-import jira from '../../Assets/skill-logos/jira.png';
-import java from '../../Assets/skill-logos/Java.png';
-import testng from '../../Assets/skill-logos/testng.png';
-import blazemeter from '../../Assets/skill-logos/blazemeter.png';
-import playwright from '../../Assets/skill-logos/playwright-logo.svg';
-import restAssured from '../../Assets/skill-logos/rest-assured.png'
+import React from "react";
+import Navbar from "../../Components/navigation/Navbar";
+import styles from "./About.module.css";
+import Image from "next/image";
+import about_image from "../../Assets/about_image-removebg.png";
+import SkillCard from "@/Components/skills/SkillCard";
+import { FaJava, FaGitSquare } from "react-icons/fa";
+import { TbSql } from "react-icons/tb";
+import { AiFillHtml5, AiFillGitlab } from "react-icons/ai";
+import { SiSelenium, SiJira, SiBlazemeter } from "react-icons/si";
+import jmeter from "../../Assets/skill-logos/Jmeter.png";
+import selenium from "../../Assets/skill-logos/selenium.png";
+import sql from "../../Assets/skill-logos/sql.png";
+import git from "../../Assets/skill-logos/git.png";
+import appium from "../../Assets/skill-logos/Appium.png";
+import jenkins from "../../Assets/skill-logos/jenkins.png";
+import rpa from "../../Assets/skill-logos/rpa.png";
+import jira from "../../Assets/skill-logos/jira.png";
+import java from "../../Assets/skill-logos/Java.png";
+import testng from "../../Assets/skill-logos/testng.png";
+import blazemeter from "../../Assets/skill-logos/blazemeter.png";
+import playwright from "../../Assets/skill-logos/playwright-logo.svg";
+import restAssured from "../../Assets/skill-logos/rest-assured.png";
+import Button from "@/Components/UI/Button/Button";
+import Link from "next/link";
+import {AiOutlineArrowRight} from 'react-icons/ai'
 function page() {
   const skill_list = [
     {
       icon: java,
-      skill: 'JAVA'
+      skill: "JAVA",
     },
     // {
     //   icon: sql,
@@ -33,48 +36,48 @@ function page() {
     // },
     {
       icon: appium,
-      skill: 'Appium'
+      skill: "Appium",
     },
     {
       icon: selenium,
-      skill: 'SELENIUM'
+      skill: "SELENIUM",
     },
     {
       icon: testng,
-      skill: 'TestNg'
+      skill: "TestNg",
     },
     {
       icon: git,
-      skill: 'Git'
+      skill: "Git",
     },
     {
       icon: jenkins,
-      skill: 'Jenkins'
+      skill: "Jenkins",
     },
     {
       icon: jira,
-      skill: 'Jira'
+      skill: "Jira",
     },
     {
       icon: blazemeter,
-      skill: 'Blazemeter'
+      skill: "Blazemeter",
     },
     {
       icon: jmeter,
-      skill: 'Jmeter'
+      skill: "Jmeter",
     },
     {
       icon: rpa,
-      skill: 'RPA'
+      skill: "RPA",
     },
     {
       icon: playwright,
-      skill: 'Playwright'
+      skill: "Playwright",
     },
     {
       icon: restAssured,
-      skill: 'Rest Assured'
-    }
+      skill: "Rest Assured",
+    },
   ];
   return (
     <>
@@ -83,7 +86,7 @@ function page() {
         <Image
           src={about_image}
           className={styles.about__img}
-          title={'customer'}
+          title={"customer"}
           alt="about-image"
         />
         <div className={styles.about__right}>
@@ -93,7 +96,7 @@ function page() {
               Over 7+ years of IT experience in both large MNC organization and
               startup company with over 3 years of experience in web-based and
               Mobile application(Android and IOS) testing and automation using
-              Selenium, testNG and Appium in Java language.
+              Selenium, testNG and Appium in Java language. 
             </p>
           </div>
           <div className={styles.about__skills}>
--- src/app/Works/Experience/Experience.jsx
+++ src/app/Works/Experience/Experience.jsx
@@ -6,62 +6,101 @@ const company = [
     name: "GOJEK",
     duration: "NOV 2021 - PRESENT",
     role: "Senior SDET",
-    skillsInvolved: ["Api automation", "feature designing", "Test case authoring", "Mobile app automation", "Web app automation",  "Performance Testing","cloud automation"],
+    skillsInvolved: [
+      "Api automation",
+      "feature designing",
+      "Test case authoring",
+      "Mobile app automation",
+      "Web app automation",
+      "Performance Testing",
+      "cloud automation",
+    ],
   },
   {
     name: "Automation Anywhere",
     duration: "Feb 2020 - NOV 2021",
     role: "Senior SDET",
-    skillsInvolved: ["Api automation", "feature designing", "Test case authoring", "Functional Integration", "Performance Testing","Load Testing","Stress Testing","Mentoring team","Release planning","Test Stratergies and Coverages", "Testing RPA"],
+    skillsInvolved: [
+      "Api automation",
+      "feature designing",
+      "Test case authoring",
+      "Functional Integration",
+      "Performance Testing",
+      "Load Testing",
+      "Stress Testing",
+      "Mentoring team",
+      "Release planning",
+      "Test Stratergies and Coverages",
+      "Testing RPA",
+    ],
   },
   {
     name: "Quikr India Pvt Ltd",
     duration: "DEC 2016 - FEB 2020",
     role: "Senior SDET",
-    skillsInvolved: ["Api automation", "feature designing", "Test case authoring", "Functional Integration", "Performance Testing","Load Testing","Stress Testing","Mentoring team","Release planning","Test Stratergies and Coverages"],
+    skillsInvolved: [
+      "Api automation",
+      "feature designing",
+      "Test case authoring",
+      "Functional Integration",
+      "Performance Testing",
+      "Load Testing",
+      "Stress Testing",
+      "Mentoring team",
+      "Release planning",
+      "Test Stratergies and Coverages",
+    ],
   },
   {
     name: "Exilant Technologies",
     duration: "DEC 2015 - DEC 2016",
     role: "Senior SDET",
-    skillsInvolved: ["Automating Beats webapp", "Testing using AEM", "Verifying AEM tool", "Validating wireframe"],
+    skillsInvolved: [
+      "Automating Beats webapp",
+      "Testing using AEM",
+      "Verifying AEM tool",
+      "Validating wireframe",
+    ],
   },
   {
-    name: "Phonton Interactive Pvt Ltd",
+    name: "Photon Interactive Pvt Ltd",
     duration: "JUNE 2014 - DEC 2015",
     role: "QA Engineer",
-    skillsInvolved: ["Mobile app testing(Android, IOS)", "Load testing", "Web automation(Selenium)", "CI CD deployment"],
+    skillsInvolved: [
+      "Mobile app testing(Android, IOS)",
+      "Load testing",
+      "Web automation(Selenium)",
+      "CI CD deployment",
+    ],
   },
 ];
 
-
 function Experience() {
-  return <div className={styles.experience__container}>
-    {company.map((company)=>{
-      return (
-        <div className={styles.experience__card} key={company.name}> 
-          <div className={styles.experience__card_left}>
-          <h2 className={styles.experience__card_name}>
-              {company.name}
-            </h2>
-            <p className={styles.experience__card_duration}>{company.duration}</p>
+  return (
+    <div className={styles.experience__container}>
+      {company.map((company) => {
+        return (
+          <div className={styles.experience__card} key={company.name}>
+            <div className={styles.experience__card_left}>
+              <h2 className={styles.experience__card_name}>{company.name}</h2>
+              <p className={styles.experience__card_duration}>
+                {company.duration}
+              </p>
+            </div>
+            <div className={styles.experience__card_right}>
+              <p className={styles.experience__card_role}>{company.role}</p>
+              <h3>Skills Involved</h3>
+              <p className={styles.experience__card_skills_container}>
+                {company.skillsInvolved.map((skill) => {
+                  return <Button>{skill}</Button>;
+                })}
+              </p>
+            </div>
           </div>
-          <div className={styles.experience__card_right}>
-           
-            <p className={styles.experience__card_role}>
-              {company.role}
-            </p>
-            <h3>Skills Involved</h3>
-            <p className={styles.experience__card_skills_container}>
-              {company.skillsInvolved.map((skill)=>{
-                return <Button>{skill}</Button>
-              })}
-            </p>
-          </div>
-        </div>
-      )
-    })}
-  </div>;
+        );
+      })}
+    </div>
+  );
 }
 
 export default Experience;
